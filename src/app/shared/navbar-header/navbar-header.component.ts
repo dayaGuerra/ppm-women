59627b139a3eb68f4182900884deb377558a4763
dayaGuerra <dayamandalaman@gmail.com>
2020-08-28 17:25:21 -0500

    Vincular a carpetas

--- src/app/shared/navbar-header/navbar-header.component.ts
+++ src/app/shared/navbar-header/navbar-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-header',
@@ -11,7 +12,7 @@ export class NavbarHeaderComponent implements OnInit {
   public isLogged = false;
   public user:any;
 
-  constructor(private authSvc: AuthService) { }
+  constructor(private authSvc: AuthService, public route: Router) { }
 
  async ngOnInit() {
     this.user = await this.authSvc.getCurrentUser();
@@ -24,5 +25,13 @@ export class NavbarHeaderComponent implements OnInit {
   onLogout() {
     this.authSvc.logout();
   }
+
+routerHome(){
+  this.route.navigate(['/windows/home']);
+}
+
+routerPerfil(){
+  this.route.navigate(['/windows/perfil']);
+}
 
 }
