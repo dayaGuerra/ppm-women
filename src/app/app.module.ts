7836049f85f265ae882a6e1e0934799dddf77b7a
dayaGuerra <dayamandalaman@gmail.com>
2020-08-29 18:08:33 -0500

    Preparando el ambiente para deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
 import { FormComponent } from './form/form.component';
 import { AcuerdoComponent } from './acuerdo/acuerdo.component';
 import { AcuerdoTwoComponent } from './acuerdo-two/acuerdo-two.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { AcuerdoTwoComponent } from './acuerdo-two/acuerdo-two.component';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireAuthModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     
   ],
   exports: [
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true ,
+  firebaseConfig : {
+    apiKey: "AIzaSyCCCGWZyL0fm1a2U3FD5A9wluYlpe_-tpY",
+    authDomain: "peppermint-women-bd.firebaseapp.com",
+    databaseURL: "https://peppermint-women-bd.firebaseio.com",
+    projectId: "peppermint-women-bd",
+    storageBucket: "peppermint-women-bd.appspot.com",
+    messagingSenderId: "1049830889386",
+    appId: "1:1049830889386:web:eaceb04cf46547ee31bdd3",
+    measurementId: "G-4N2D568XJ4"
+  }
 };
