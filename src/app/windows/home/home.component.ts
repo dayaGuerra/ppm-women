6a37f592722e39aaa995707d144e43c5939fc5d6
dayaGuerra <dayamandalaman@gmail.com>
2020-08-28 21:29:32 -0500

    cambios de tamaños

--- src/app/windows/home/home.component.ts
+++ src/app/windows/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public isLogged = false;
+  public user:any;
 
-  constructor() { }
+  constructor(private authSvc: AuthService, public route: Router) { }
 
-  ngOnInit(): void {
+
+  async ngOnInit() {
+    this.user = await this.authSvc.getCurrentUser();
+    console.log(this.user);
+    if (this.user) {
+      this.isLogged = true;
+    }
   }
+
+  onLogout() {
+    this.authSvc.logout();
+  }
+
+routerHome(){
+  this.route.navigate(['/windows/home']);
+}
+
+routerPerfil(){
+  this.route.navigate(['/windows/perfil']);
+}
 
 }
